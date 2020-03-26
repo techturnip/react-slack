98753a42b6e3bd322ed56b2e43543557024bb25b
Tyler Turnipseed <tyler@techturnip.us>
2020-03-26 15:11:35 -0400

    add additional comments for register component

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Grid,
   Form,
@@ -7,50 +7,63 @@ import {
   Header,
   Message,
   Icon
-} from "semantic-ui-react";
-import { Link } from "react-router-dom";
-import firebase from "../../firebase";
-import md5 from "md5";
+} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import firebase from '../../firebase';
+import md5 from 'md5';
 
 class Register extends React.Component {
+  //-----------------------------------------------------|
+  // STATE ----------------------------------------------|
+  //=====================================================|
   state = {
-    username: "",
-    email: "",
-    password: "",
-    passwordConfirmation: "",
-    errors: [],
-    loading: false,
-    usersRef: firebase.database().ref("users")
+    username: '', // username form value
+    email: '', // set by email form value
+    password: '', // set by password form value
+    passwordConfirmation: '', // set by passwordConf.. form value
+    errors: [], // set by error handling fn's
+    loading: false, // handles submit btn loading state
+    usersRef: firebase.database().ref('users') // stores db reference to the users table
   };
-
-  // form validation
+  //-----------------------------------------------------|
+  // FORM VALIDATION ------------------------------------|
+  //=====================================================|
+  // Validate Form w/ Error Handling
   isFormValid = () => {
+    // initialize an errors array
     let errors = [];
+    // initialize error var for error messages
     let error;
 
+    // checks if the form is empty
     if (this.isFormEmpty(this.state)) {
+      // if form is empty then:
       // set error message
-      error = { message: "Fill in all fields" };
-      // set error state
+      error = { message: 'Fill in all fields' };
+      // set error state by concatenating the error msg
+      // onto the errors []
       this.setState({ errors: errors.concat(error) });
-
       // return false
       return false;
+
+      // check if password is valid
     } else if (!this.isPasswordValid(this.state)) {
       // set error message
-      error = { message: "Password is invalid" };
-      // set error state
+      error = { message: 'Password is invalid' };
+      // set error state by concatenating the error msg
+      // onto the errors []
       this.setState({ errors: errors.concat(error) });
-
       // return false
       return false;
     } else {
       return true;
     }
   };
-
-  // check for empty form
+  //-----------------------------------------------------|
+  // Empty Form Check
   isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    // if len === 0 for any of the conditions, return true
+    // confirming that there is an empty field in the form
     return (
       !username.length ||
       !email.length ||
@@ -58,38 +71,69 @@ class Register extends React.Component {
       !passwordConfirmation.length
     );
   };
-
-  // check for valid password
+  //-----------------------------------------------------|
+  // Password Validation Check
   isPasswordValid = ({ password, passwordConfirmation }) => {
     if (password.length < 6 || passwordConfirmation.length < 6) {
+      // password & confirmation must be at least 6 chars
+      // return false if condition not met
       return false;
     } else if (password !== passwordConfirmation) {
+      // password must match the confirmation
+      // return false if condition not met
       return false;
     } else {
+      // password & confirmation are at least 6 chars & matching
+      // return true - password is valid
       return true;
     }
   };
-
-  // display errors
+  //-----------------------------------------------------|
+  // ERROR HANDLERS -------------------------------------|
+  //=====================================================|
+  // Display Errors
+  // Desc: fn maps the errors array, displaying <p> tags that
+  // contain the error messages.
   displayErrors = errors =>
     errors.map((error, i) => <p key={i}>{error.message}</p>);
-
-  // change handler
+  //-----------------------------------------------------|
+  // Apply Input Error Class
+  // Desc: fn conditionally applies the error class to the
+  // corresponding input field in the event an error is thrown
+  handleInputError = (errors, inputName) => {
+    return errors.some(error => error.message.toLowerCase().includes(inputName))
+      ? 'error'
+      : '';
+  };
+  //-----------------------------------------------------|
+  // EVENT HANDLERS -------------------------------------|
+  //=====================================================|
+  // Change Handler
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-
-  // submit handler
+  //-----------------------------------------------------|
+  // Submit Handler
   handleSubmit = event => {
+    // prevent default submit btn behavior
     event.preventDefault();
 
+    // check if the form is valid
     if (this.isFormValid()) {
+      // if valid:
+      // empty errors arr and set loading state to true
       this.setState({ errors: [], loading: true });
+
+      // send the form data to firebase for registration
       firebase
         .auth()
-        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .createUserWithEmailAndPassword(this.state.email, this.state.password) // create user with email and pwd
         .then(createdUser => {
+          // if user creation succeeds:
           console.log(createdUser);
+
+          // call updateProfile passing the username to displayName
+          // and generating a gravatar img
           createdUser.user
             .updateProfile({
               displayName: this.state.username,
@@ -98,12 +142,19 @@ class Register extends React.Component {
               )}?d=identicon`
             })
             .then(() => {
+              // if update profile succeeds:
+              // save user
               this.saveUser(createdUser).then(() => {
-                console.log("user saved");
+                // if save user succeeds:
+                console.log('user saved');
               });
             })
             .catch(err => {
+              // catch error if update profile fails
               console.error(err);
+
+              // update state, passing in the err message to
+              // errors [] and setting loading to false
               this.setState({
                 errors: this.state.errors.concat(err),
                 loading: false
@@ -111,7 +162,10 @@ class Register extends React.Component {
             });
         })
         .catch(err => {
+          // catch error if create user fails
           console.error(err);
+          // update state, passing in the err message to
+          // errors [] and setting loading to false
           this.setState({
             errors: this.state.errors.concat(err),
             loading: false
@@ -119,21 +173,19 @@ class Register extends React.Component {
         });
     }
   };
-
+  //-----------------------------------------------------|
+  // Helper function to save the user
   saveUser = createdUser => {
     return this.state.usersRef.child(createdUser.user.uid).set({
       name: createdUser.user.displayName,
       avatar: createdUser.user.photoURL
     });
   };
-
-  handleInputError = (errors, inputName) => {
-    return errors.some(error => error.message.toLowerCase().includes(inputName))
-      ? "error"
-      : "";
-  };
-
+  //-----------------------------------------------------|
+  // RENDER ---------------------------------------------|
+  //=====================================================|
   render() {
+    // destructure state obj
     const {
       username,
       email,
@@ -142,7 +194,7 @@ class Register extends React.Component {
       errors,
       loading
     } = this.state;
-
+    // return JSX
     return (
       <Grid textAlign="center" verticalAlign="middle" className="app">
         <Grid.Column style={{ maxWidth: 450 }}>
@@ -160,7 +212,7 @@ class Register extends React.Component {
                 placeholder="Username"
                 onChange={this.handleChange}
                 value={username}
-                className={this.handleInputError(errors, "username")}
+                className={this.handleInputError(errors, 'username')}
                 type="text"
               />
               <Form.Input
@@ -171,7 +223,7 @@ class Register extends React.Component {
                 placeholder="Email Address"
                 onChange={this.handleChange}
                 value={email}
-                className={this.handleInputError(errors, "email")}
+                className={this.handleInputError(errors, 'email')}
                 type="email"
               />
               <Form.Input
@@ -182,7 +234,7 @@ class Register extends React.Component {
                 placeholder="Password"
                 onChange={this.handleChange}
                 value={password}
-                className={this.handleInputError(errors, "password")}
+                className={this.handleInputError(errors, 'password')}
                 type="password"
               />
               <Form.Input
@@ -193,13 +245,13 @@ class Register extends React.Component {
                 placeholder="Password Confirmation"
                 onChange={this.handleChange}
                 value={passwordConfirmation}
-                className={this.handleInputError(errors, "password")}
+                className={this.handleInputError(errors, 'password')}
                 type="password"
               />
 
               <Button
                 disabled={loading}
-                className={loading ? "loading" : ""}
+                className={loading ? 'loading' : ''}
                 color="orange"
                 fluid
                 size="large"
@@ -223,4 +275,5 @@ class Register extends React.Component {
   }
 }
 
+// export component
 export default Register;
