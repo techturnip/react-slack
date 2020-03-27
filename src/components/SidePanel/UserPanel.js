7689834099e3fd1672f43dc10b63be381ef73362
Tyler Turnipseed <tyler@techturnip.us>
2020-03-27 02:10:04 -0400

    added avatar to user dropdown

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -11,7 +11,7 @@ import firebase from '../../firebase';
 //=====================================================|
 // COMPONENTS IMPORTS ---------------------------------|
 //-----------------------------------------------------|
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 //=====================================================|
 // USER PANEL COMPONENT ===============================|
 //=====================================================|
@@ -54,8 +54,9 @@ class UserPanel extends React.Component {
       .then(() => console.log('signed out!'));
   };
   //---------------------------------------------------|
-
   render() {
+    const { user } = this.state;
+
     return (
       <Grid style={{ background: '#4c3c4c' }}>
         <Grid.Column>
@@ -65,15 +66,20 @@ class UserPanel extends React.Component {
               <Icon name="code" />
               <Header.Content>TechChat</Header.Content>
             </Header>
-          </Grid.Row>
 
-          {/* User Dropdown */}
-          <Header style={{ padding: '0.25em' }} as="h4" inverted>
-            <Dropdown
-              trigger={<span>{this.state.user.displayName}</span>}
-              options={this.dropdownOptions()}
-            />
-          </Header>
+            {/* User Dropdown */}
+            <Header style={{ padding: '0.25em' }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
+          </Grid.Row>
         </Grid.Column>
       </Grid>
     );
