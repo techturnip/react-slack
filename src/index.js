25d54122c40375f8845af18293244b158922de27
Tyler Turnipseed <tyler@techturnip.us>
2020-03-26 20:33:52 -0400

    added comments, hooked up set user action

--- src/index.js
+++ src/index.js
@@ -1,32 +1,59 @@
+//=====================================================|
+// IMPORTS ============================================|
+//=====================================================|
+// REACT IMPORTS --------------------------------------|
+//-----------------------------------------------------|
 import React from 'react';
 import ReactDOM from 'react-dom';
+//=====================================================|
+// COMPONENTS IMPORTS ---------------------------------|
+//-----------------------------------------------------|
 import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+//=====================================================|
+// MISC IMPORTS ---------------------------------------|
+//-----------------------------------------------------|
 import registerServiceWorker from './registerServiceWorker';
+//=====================================================|
+// STYLE IMPORTS --------------------------------------|
+//-----------------------------------------------------|
 import 'semantic-ui-css/semantic.min.css';
+//=====================================================|
+// FIREBASE IMPORTS -----------------------------------|
+//-----------------------------------------------------|
 import firebase from './firebase';
-
+//=====================================================|
+// ROUTER IMPORTS -------------------------------------|
+//-----------------------------------------------------|
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   withRouter
 } from 'react-router-dom';
-
+//=====================================================|
+// REDUX IMPORTS --------------------------------------|
+//-----------------------------------------------------|
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, connect } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-
-const store = createStore(() => {}, composeWithDevTools());
-
+import rootReducer from './reducers';
+import { setUser } from './actions';
+//=====================================================|
+// SETUP STORE ========================================|
+//=====================================================|
+const store = createStore(rootReducer, composeWithDevTools());
+//=====================================================|
+// SETUP ROOT COMPONENT ===============================|
+//=====================================================|
 class Root extends React.Component {
   componentDidMount() {
     // setup listener for Auth to push user from Auth flow
-    // to the application root
+    // to the application root if logged in
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        // setUser(user);
+        this.props.setUser(user);
         this.props.history.push('/');
       }
     });
@@ -42,11 +69,15 @@ class Root extends React.Component {
     );
   }
 }
-
+//=====================================================|
+// SETUP ROUTER HOC ===================================|
+//=====================================================|
 // Wrap Root with withRouter HOC for access to history.push
 // inside of the component lifecycle method componentDidMount()
-const RootWithAuth = withRouter(Root);
-
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+//=====================================================|
+// RENDER APP =========================================|
+//=====================================================|
 // Wrap the HOC in Router inside of the render
 ReactDOM.render(
   <Provider store={store}>
@@ -56,4 +87,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+//=====================================================|
 registerServiceWorker();
