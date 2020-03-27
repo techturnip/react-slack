472ef85b7ebd0f820d6c18663e56eefccd651664
Tyler Turnipseed <tyler@techturnip.us>
2020-03-27 01:40:39 -0400

    flesh out markup for UserPanel

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -7,11 +7,31 @@ import React from 'react';
 //=====================================================|
 // COMPONENTS IMPORTS ---------------------------------|
 //-----------------------------------------------------|
-import { Grid, Header, Icon } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 //=====================================================|
 // USER PANEL COMPONENT ===============================|
 //=====================================================|
 class UserPanel extends React.Component {
+  dropdownOptions = () => [
+    {
+      key: 'user',
+      text: (
+        <span>
+          Signed in as <strong>User</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    {
+      key: 'avatar',
+      text: <span>Change Avater</span>
+    },
+    {
+      key: 'signout',
+      text: <span>Sign Out</span>
+    }
+  ];
+
   render() {
     return (
       <Grid style={{ background: '#4c3c4c' }}>
@@ -23,6 +43,14 @@ class UserPanel extends React.Component {
               <Header.Content>TechChat</Header.Content>
             </Header>
           </Grid.Row>
+
+          {/* User Dropdown */}
+          <Header style={{ padding: '0.25em' }} as="h4" inverted>
+            <Dropdown
+              trigger={<span>User</span>}
+              options={this.dropdownOptions()}
+            />
+          </Header>
         </Grid.Column>
       </Grid>
     );
