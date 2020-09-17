f617e4327738006f7b79a036a849aef195e4322d
Tyler Turnipseed <tyler@techturnip.us>
2020-09-17 17:55:27 -0400

    add channel support in global state

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,24 +3,35 @@
 //=====================================================|
 // IMPORTS ============================================|
 //=====================================================|
-import * as actionTypes from './types';
+import * as actionTypes from "./types";
 //=====================================================|
 // ACTIONS ============================================|
 //=====================================================|
 // USER ACTIONS ---------------------------------------|
 //-----------------------------------------------------|
-export const setUser = user => {
+export const setUser = (user) => {
   return {
     type: actionTypes.SET_USER,
     payload: {
-      currentUser: user
-    }
+      currentUser: user,
+    },
   };
 };
 //-----------------------------------------------------|
 export const clearUser = () => {
   return {
-    type: actionTypes.CLEAR_USER
+    type: actionTypes.CLEAR_USER,
+  };
+};
+//-----------------------------------------------------|
+// CHANNEL ACTIONS ------------------------------------|
+//-----------------------------------------------------|
+export const setCurrentChannel = (channel) => {
+  return {
+    type: actionTypes.SET_CURRENT_CHANNEL,
+    payload: {
+      currentChannel: channel,
+    },
   };
 };
 //=====================================================|
--- src/actions/types.js
+++ src/actions/types.js
@@ -3,6 +3,10 @@
 //=====================================================|
 // USER ACTION TYPES ----------------------------------|
 //-----------------------------------------------------|
-export const SET_USER = 'SET_USER';
-export const CLEAR_USER = 'CLEAR_USER';
+export const SET_USER = "SET_USER";
+export const CLEAR_USER = "CLEAR_USER";
+//-----------------------------------------------------|
+// CHANNEL ACTION TYPES -------------------------------|
+//-----------------------------------------------------|
+export const SET_CURRENT_CHANNEL = "SET_CURRENT_CHANNEL";
 //=====================================================|
