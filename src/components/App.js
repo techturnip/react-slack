55323c3d224b4ef0dece14a0ec30e16b845bbb8d
Tyler Turnipseed <tyler@techturnip.us>
2020-09-17 20:35:27 -0400

    display messages

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,37 @@
 //=====================================================|
 // REACT IMPORTS --------------------------------------|
 //-----------------------------------------------------|
-import React from 'react';
+import React from "react";
 //=====================================================|
 // REDUX IMPORTS --------------------------------------|
 //-----------------------------------------------------|
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 //=====================================================|
 // COMPONENTS IMPORTS ---------------------------------|
 //-----------------------------------------------------|
-import { Grid } from 'semantic-ui-react';
-import ColorPanel from './ColorPanel/ColorPanel';
-import Messages from './Messages/Messages';
-import MetaPanel from './MetaPanel/MetaPanel';
-import SidePanel from './SidePanel/SidePanel';
+import { Grid } from "semantic-ui-react";
+import ColorPanel from "./ColorPanel/ColorPanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+import SidePanel from "./SidePanel/SidePanel";
 //=====================================================|
 // STYLE IMPORTS --------------------------------------|
 //-----------------------------------------------------|
-import './App.css';
+import "./App.css";
 //=====================================================|
 // APP COMPONENT ======================================|
 //=====================================================|
-const App = ({ currentUser }) => (
-  <Grid columns="equal" className="app" style={{ background: '#eee' }}>
+const App = ({ currentUser, currentChannel }) => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel currentUser={currentUser} />
+    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
-      <Messages />
+      <Messages
+        key={currentChannel && currentChannel.id}
+        currentChannel={currentChannel}
+        currentUser={currentUser}
+      />
     </Grid.Column>
 
     <Grid.Column width={4}>
@@ -40,8 +44,9 @@ const App = ({ currentUser }) => (
 //=====================================================|
 // MAP STATE TO PROPS =================================|
 //=====================================================|
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+  currentChannel: state.channel.currentChannel,
 });
 //=====================================================|
 // EXPORT COMPONENT ===================================|
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -5,20 +5,71 @@ import React from "react";
 import { Segment, Comment } from "semantic-ui-react";
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
+import firebase from "../../firebase";
+import Message from "./Message";
 //=====================================================|
 // COLOR PANEL COMPONENT ==============================|
 //=====================================================|
 class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser,
+    messages: [],
+    messagesLoading: true,
+  };
+
+  componentDidMount() {
+    const { channel, user } = this.state;
+
+    if (channel && user) {
+      this.addListeners(channel.id);
+    }
+  }
+
+  addListeners = (channelId) => {
+    this.addMessageListener(channelId);
+  };
+
+  addMessageListener = (channelId) => {
+    let loadedMessages = [];
+    this.state.messagesRef.child(channelId).on("child_added", (snap) => {
+      loadedMessages.push(snap.val());
+      this.setState({
+        messages: loadedMessages,
+        messagesLoading: false,
+      });
+    });
+  };
+
+  displayMessages = (messages) =>
+    messages.length > 0 &&
+    messages.map((message) => (
+      <Message
+        key={message.timestamp}
+        message={message}
+        user={this.state.user}
+      />
+    ));
+
   render() {
+    const { messagesRef, messages, channel, user } = this.state;
+
     return (
       <>
         <MessagesHeader />
 
         <Segment>
-          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+          <Comment.Group className="messages">
+            {this.displayMessages(messages)}
+          </Comment.Group>
         </Segment>
 
-        <MessageForm />
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
       </>
     );
   }
